a0813102e7b87ea20dd50c102ff1c0100c48105b
Daniel Westheide <daniel.westheide@innoq.com>
2014-02-13 14:36:36 +0100

    Reply to sender with appropriate event messages

--- app/service/task/Task.scala
+++ app/service/task/Task.scala
@@ -1,6 +1,9 @@
 package service.task
 
-import akka.actor.{ FSM, Actor }
+import akka.actor.{ Props, FSM, Actor }
+import service.task.Task.Protocol._
+import service.task.Task.Protocol.TaskClaimed
+import service.task.Task.Protocol.TaskInitialized
 
 class Task extends Actor with FSM[TaskState, Data] {
 
@@ -8,33 +11,51 @@ class Task extends Actor with FSM[TaskState, Data] {
 
   when(Created) {
     case Event(Init(input), data) =>
-      goto(Ready) using InitialData(input)
+      goto(Ready) using InitialData(input) replying TaskInitialized(input)
   }
 
   when(Ready) {
     case Event(Claim(userId), InitialData(input)) =>
-      goto(Reserved) using ClaimedData(input, userId)
+      goto(Reserved) using ClaimedData(input, userId) replying TaskClaimed
   }
 
   when(Reserved) {
     case Event(Start, data) =>
-      goto(InProgress) using data
+      goto(InProgress) using data replying TaskStarted
     case Event(Release, ClaimedData(input, _)) =>
-      goto(Ready) using InitialData(input)
+      goto(Ready) using InitialData(input) replying TaskReleased
   }
 
   when(InProgress) {
     case Event(Complete(result), ClaimedData(input, userId)) =>
-      goto(Completed) using CompletedData(input, userId, result)
+      goto(Completed) using CompletedData(input, userId, result) replying TaskCompleted(result)
     case Event(Stop, _) =>
-      goto(Reserved)
+      goto(Reserved) replying TaskStopped
   }
 
   whenUnhandled {
     case Event(Skip, _) =>
-      goto(Obsolete) using EmptyData
+      goto(Obsolete) using EmptyData replying TaskSkipped
+    case Event(cmd: Command, _) =>
+      stay replying InvalidCommandRejected(cmd, stateName)
   }
 
   initialize()
 
+}
+
+object Task {
+
+  def props() = Props[Task]
+
+  object Protocol {
+    case class TaskInitialized(input: TaskData)
+    case class TaskClaimed(assigneeId: String)
+    case object TaskStarted
+    case object TaskReleased
+    case class TaskCompleted(result: TaskData)
+    case object TaskStopped
+    case object TaskSkipped
+    case class InvalidCommandRejected(cmd: Command, state: TaskState)
+  }
 }
