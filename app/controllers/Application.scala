07efd6bb8b72d20a4ef17fd211dbd4c575bd1c8d
Tobias Neef <tobnee@gmail.com>
2014-02-13 17:32:54 +0100

    added json home document

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,18 @@ package controllers
 import play.api._
 import play.api.mvc._
 import plugin.EventsourcedPlugin
+import play.api.libs.json.Json
 
 object Application extends Controller {
 
-  def index = Action {
+  def index = Action { implicit request =>
+    render {
+      case Accepts.Html() => html
+      case Accepts.Json() => json
+    }
+  }
+
+  def html = {
     val a = play.api.Play.current.plugin[EventsourcedPlugin] match {
       case Some(plugin) => plugin.eventsourcedExtension
       case None => throw new Exception("No eventsourced plugin registered")
@@ -14,4 +22,12 @@ object Application extends Controller {
     Ok(views.html.index())
   }
 
+  def json(implicit request: Request[_]) = {
+    val url = routes.Tasks.createTask.absoluteURL()
+    val json = Json.toJson(
+      Map("resources" ->
+        Map("tasks" ->
+          Map("href" -> url))))
+    Ok(json)
+  }
 }
